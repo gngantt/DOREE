759884a58fdc89f9c8390568498ad03e3e6324f2
Gabby Gantt <gngantt@gmail.com>
2019-05-28 19:19:19 -0400

    Fixed Syntax

--- LED.py
+++ LED.py
@@ -1,11 +1,11 @@
 """
 Test code so I can see how LED's work on a Pi
 """
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(FALSE)
+GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
 
 print "LED on"
@@ -13,6 +13,6 @@ print "LED on"
 GPIO.output(18, GPIO.HIGH)
 time.sleep(1)
 
-Print "LED off"
+print "LED off"
 
 GPIO.output(18, GPIO.LOW)
