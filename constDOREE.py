67d73fc5b7bbdfa29d05c0c1b723575b0d30ac06
Janine <thomas.j18@hotmail.com>
2019-06-06 19:31:21 -0400

    Added a third signal so now there is a critical, warning and ready level

--- constDOREE.py
+++ constDOREE.py
@@ -3,8 +3,10 @@ DOREE - Gabby Gantt, Jacob Lebreux, Janine Thomas
 
 This file is used to receive the programmed warning and ready levels for DOREE
 
-WARN - the warning level where, when reached, DOREE will need to abort mission
-to go charge her battery
+CRIT - the critical level where, when reached, DOREE will abort mission to go
+charge her battery
+WARN - the warning level where, when reached, DOREE will warn navigation to
+get ready to go charge her battery
 READY - the ready level where, when reached, DOREE will need to stop charging
 and continue with her mission
 '''
@@ -12,9 +14,11 @@ and continue with her mission
 okay = False
 
 while not okay:
+    CRIT = float(input('Please enter the desired critical level: '))
     WARN = float(input('Please enter the desired warning level: '))
     READY = float(input('Please enter the desired ready level: '))
 
+    print('The critical level for DOREE is ', CRIT)
     print('The warning level for DOREE is ', WARN)
     print('The ready level for DOREE is ', READY)
 
@@ -22,4 +26,4 @@ while not okay:
 
     okay = True if ((question == 'Y') or (question == 'y')) else False
 
-print('Warning and Ready levels have been set!\n')
+print('Critical, Warning, and Ready levels have been set!\n')
