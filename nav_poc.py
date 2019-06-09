490424060a9da84f1268d6f87386f7eeb84fa05f
Gabby Gantt <gngantt@gmail.com>
2019-06-09 12:10:16 -0400

    update

--- nav_poc.py
+++ nav_poc.py
@@ -9,32 +9,59 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False) #fixed dumb warning
 
+#Inputs
 PIN_POWER_WARNING = 26
 PIN_POWER_CRITICAL = 19
-#PIN_DETECTION = 1 
+PIN_DETECTION = 13
+
+#Outputs
+PIN_SHUTDOWN_DETECTION = 5
 
    
 GPIO.setup(PIN_POWER_WARNING, GPIO.IN)
 GPIO.setup(PIN_POWER_CRITICAL, GPIO.IN)
-#GPIO.setup(PIN_DETECTION, GPIO.IN)
+GPIO.setup(PIN_DETECTION, GPIO.IN)
+GPIO.setup(PIN_SHUTDOWN_DETECTION, GPIO.OUT)
 
+GPIO.output(PIN_SHUTDOWN_DETECTION, 0)
+
+timeUp = 1
 
 while(1):
   
-   if(GPIO.input(PIN_POWER_WARNING) == 1):
+    if(GPIO.input(PIN_POWER_WARNING) == 1):
         print(GPIO.input(PIN_POWER_WARNING))
         print("Warning\n")
         
-        #start timer for 10 mins? if noting found from detection send signalto stop
+        timeUp += 1
+        print(timeUp)
+        
+        if(timeUp == 10):
+            GPIO.output(PIN_SHUTDOWN_DETECTION, 1)
+            timeUp = 1
+            
+        elif(timeUp > 10):
+            timeUp = 1
+            
     
-   if(GPIO.input(PIN_POWER_CRITICAL) == 1):
+    if(GPIO.input(PIN_POWER_CRITICAL) == 1):
         print(GPIO.input(PIN_POWER_CRITICAL))
         print("Critical\n")
         
         #dont start timer - send robot up
         #check for detection off (for loop - if noting at end go up anyway)
-   
+      
+    if(GPIO.input(PIN_POWER_WARNING) == 0 and GPIO.input(PIN_POWER_CRITICAL) == 0):
+        GPIO.output(PIN_SHUTDOWN_DETECTION, 0)
+        
+      
+      
+    if(GPIO.input(PIN_DETECTION) == 1):
+        print(GPIO.input(PIN_DETECTION))
+        print("Detected Object\n") 
     
-        time.sleep(1)
+    time.sleep(1)
+   
 
