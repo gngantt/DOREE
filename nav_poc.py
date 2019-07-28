66f1d6ab0f40813c2722ff4bb2b732b755c5e8c2
Gantt <ganttg@wit.edu>
2019-07-28 11:46:08 -0400

    update

--- nav_poc.py
+++ nav_poc.py
@@ -27,6 +27,56 @@ GPIO.setup(PIN_SHUTDOWN_DETECTION, GPIO.OUT)
 
 GPIO.output(PIN_SHUTDOWN_DETECTION, 0)
 
+
+#GPIO.setmode(GPIO.BOARD)
+
+#initiates GPIO pins on Pi
+PIN_TRIGGER = 7
+PIN_ECHO = 11
+
+#sets pins to input or output
+GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+GPIO.setup(PIN_ECHO, GPIO.IN)
+
+GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+
+
+
+def acoustic():
+    try:
+
+
+            print ("Waiting for sensor to settle")
+
+            time.sleep(2)
+
+            print ("Calculationg distance")
+
+            GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+
+            time.sleep(0.00001)
+
+
+            GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+            while GPIO.input(PIN_ECHO)==0:
+                pulse_start_time = time.time()
+            while GPIO.input(PIN_ECHO)==1:
+                pulse_end_time = time.time()
+
+            pulse_duration = pulse_end_time - pulse_start_time
+            distance = round(pulse_duration * 17150, 2)
+            #print "Distance:", distance, "cm"
+            return distance
+
+    finally:
+        GPIO.cleanup()
+
+
+
+
+
 warningAck = 0
 timeUp = 0
 
@@ -51,7 +101,6 @@ while(1):
             print("Timer signal sent")
             #timeUp = 1
 
-
     if(GPIO.input(PIN_POWER_CRITICAL) == 1):
         #print(GPIO.input(PIN_POWER_CRITICAL))
         print("Critical signal detected\n")
@@ -64,10 +113,16 @@ while(1):
         warningAck = 0
         timeUp = 0
 
-
-
     if(GPIO.input(PIN_DETECTION) == 1):
         print(GPIO.input(PIN_DETECTION))
-        print("Detected Object\n")
+        print("Detected life form\n")
+
+    x = acoustic()
+
+    if(x < 15):
+        print("Life form")
+
+
+
 
     time.sleep(1)
